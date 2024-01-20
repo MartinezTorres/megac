c5d47873c306ea8ac6c30b61c9a3c1c457601b30
Manuel Martinez <salutte@gmail.com>
2024-01-20 19:23:19 +0100

    added optimizer and generator

--- src/parser.h
+++ src/parser.h
@@ -1,22 +1,2 @@
 #pragma once
-#include <tokenizer.h>
-
-#include <optional>
-
-struct SyntaxTree {
-	
-
-	using TI = std::vector<Token>::const_iterator;
-	
-	TI first, last;
-	std::string symbol;
-	
-	std::vector<SyntaxTree> children;
-	
-	SyntaxTree(TI begin, std::string symbol);
-
-	SyntaxTree(SourceFile &file, std::string root = "start");
-	
-	std::string to_string(std::string prefix = "") const;
-};
-
+#include <ast.h>
